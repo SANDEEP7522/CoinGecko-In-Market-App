afc628b5be7a33fc7218e2478a6f4e1965322747
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-08-19 17:04:50 +0530

    API call and UseEffect use

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CoinTable from './Components/CoinTable/CoinTable'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Sandeep</h1>
+   
+    <CoinTable />
     </>
   )
 }
---
+++ src/Components/CoinTable/CoinTable.jsx
@@ -0,0 +1,50 @@
+// import React, { Component } from 'react'
+
+import { useEffect, useState } from "react";
+function CoinTable() {
+ 
+  const [count, setCount] = useState(0);
+
+  const [flag, setflag] = useState(false);
+
+    async function downlode() {
+        const responce = await fetch ("https://jsonplaceholder.typicode.com/todos/2") 
+        console.log(responce);
+        const result = await responce.joson;
+        console.log(result);
+            
+    }
+
+ useEffect(() => {
+    downlode();
+ },[count]);
+
+ useEffect(() => {
+    console.log("Flag is change");
+ },[flag]);
+
+ useEffect(() => {
+    console.log("Flage is not Change");  
+ },);
+
+ useEffect(() => {
+    console.log("flage is change & !change ");
+    
+ },[count, flag]);
+
+
+    return (
+      <>
+        <h1>coin</h1>
+        {count}
+        <br />
+        <button onClick={() => setCount(count +1)}>Increment</button>
+        <br />
+        {flag && <div>flag is true</div> }
+        <br />
+        <button onClick={() => setflag(!flag)}>Toggle</button>
+      </>
+    );
+ }
+
+export default CoinTable;
