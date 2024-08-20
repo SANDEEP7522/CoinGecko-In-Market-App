8d5f5c81d72c55f492a367026189206d812f9bff
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-08-20 23:38:50 +0530

    Add coin table ui with cache

--- src/Components/Serviecs/FetchCoinData.js
+++ src/Components/Serviecs/FetchCoinData.js
@@ -4,26 +4,18 @@ import AxiosIncetance from "../../Helpers/AxiosIncetance";
 export async function FetchCoinData(page=1, currency='usd') {
 
     const perpage = 10; 
+ try{
+     const responce = await AxiosIncetance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perpage}&page=${page} `);
 
-
-    
-    try{
-        const responce = await AxiosIncetance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perpage}&page=${page} `);
-
-        console.log(responce);
-        return responce;
+        // console.log(responce);
+        return responce.data;
     }
-
-    catch(error){
+  catch(error){
         console.log(error);
         return null;
-        
-
     }
 
 }
-
-
 export default FetchCoinData;
 
 
